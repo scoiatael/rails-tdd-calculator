c9ac211b1d366de08e8293c9393cf055c2194f13
LC <eternal.kadir@gmail.com>
2014-12-14 21:24:43 +0000

    changed add method of math_controller

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,4 +1,13 @@
 class MathController < ApplicationController
   def add
+    @value = first_arg.to_f + snd_arg.to_f
+  end
+
+  def first_arg
+    params.require :arg1
+  end
+
+  def snd_arg
+    params.require :arg2
   end
 end
