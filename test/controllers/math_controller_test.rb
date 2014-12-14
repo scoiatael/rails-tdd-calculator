6533eead124ab2a1d43b790a1e1542b174408859
LC <eternal.kadir@gmail.com>
2014-12-14 21:24:12 +0000

    add controller test

--- test/controllers/math_controller_test.rb
+++ test/controllers/math_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class MathControllerTest < ActionController::TestCase
-  test "should get add" do
-    get :add
-    assert_response 400, "Add requires arguments"
+  test "add without arguments" do
+    assert_raise ActionController::ParameterMissing do
+      get :add
+    end
+  end
+
+  test "add with arguments" do
+    num1 = rand(100)
+    num2 = rand(100)
+    get :add, { arg1: num1, arg2: num2 }
+    assert_equal (num1+num2), assigns(:value)
   end
 
 end
