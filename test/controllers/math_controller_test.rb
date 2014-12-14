d83917c38bdbe392de8b05dd56130da51eb91045
LC <eternal.kadir@gmail.com>
2014-12-14 21:07:02 +0000

    math controller test

--- test/controllers/math_controller_test.rb
+++ test/controllers/math_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MathControllerTest < ActionController::TestCase
   test "should get add" do
     get :add
-    assert_response :success
+    assert_response 400, "Add requires arguments"
   end
 
 end
