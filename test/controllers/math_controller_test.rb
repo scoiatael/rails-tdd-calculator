4e5c1bd5eca1eb7440424c5b5e85b593f3175517
LC <eternal.kadir@gmail.com>
2014-12-14 22:19:46 +0000

    more tests for math controller, fixtures for calculator model

--- test/controllers/math_controller_test.rb
+++ test/controllers/math_controller_test.rb
@@ -1,22 +1,42 @@
 require 'test_helper'
 
 class MathControllerTest < ActionController::TestCase
+  def raisesParamMissing
+    assert_raise ActionController::ParameterMissing, &Proc.new
+  end
+
   test "get add without arguments" do
-    assert_raise ActionController::ParameterMissing do
-      get :add
-    end
+    raisesParamMissing { get :add }
+    raisesParamMissing { get :add, id: 2 }
+    raisesParamMissing { get :add, value: 2 }
   end
 
   test "get add with value arguments" do
     10.times do
-      num1 = rand(100)
-      num2 = rand(100)
-      get :add, { value: num1, value2: num2 }
-      assert_equal (num1+num2), assigns(:value)
+      randomize2_num!
+      get :add, { value: @num, value2: @num2 }
+      assert_equal (@num + @num2), assigns(:value)
     end
   end
 
-  test "get add with id arguments" do
+  test "get add with id arguments assigns calculator" do
+    randomize_num!
+    get :add, { value: @num, id: 1 }
+    assert_equal Calculator.find(1), assigns(:calculator)
+  end
+
+  test "get add with id arguments assigns correct value" do
+    10.times do
+      randomize_num!
+      get :add, { value: @num, id: 1 }
+      assert_equal (@num + Calculator.find(1).value), assigns(:value)
+    end
+  end
+
+  test "post add without arguments" do
+    raisesParamMissing { post :add }
+    raisesParamMissing { post :add, id: 2 }
+    raisesParamMissing { post :add, value: 2 }
   end
 
   test "post add with id arguments" do
